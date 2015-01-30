533cc6f744acf12f77a06171a23049e1036dc5f6
Kendra <kdodds@gmail.com>
2015-01-30 00:43:38 -0700

    add activemodel serializer

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,31 +2,34 @@ class NotesController < ApplicationController
   def index
     @notes = Note.all
     render json: @notes
+    # or you just need:
+    # render json: Note.all
   end
 
   def create
+    # at.note = Note.create(params.require(:note).permit(:title, :body))
     note_params = params.require(:note).permit(:title, :body)
     @note = Note.new(note_params)
     @note.save
-    render json: {note: @note}
+    render json: @note
   end
 
   def show
     @note = Note.find(params[:id])
-    render json: {note: @note}
+    render json: @note
   end
 
   def update
     note_params = params.require(:note).permit(:title, :body)
     @note = Note.find(params[:id])
     @note.update(note_params)
-    render json: {note: @note}
+    render json: @note
   end
 
   def destroy
     @note = Note.find(params[:id])
     @note.destroy
-    render json: {note: @note}
+    render json: @note
   end
 
 end
